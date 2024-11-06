99bbd8a27d1935cfb780d9ae6d2453543546266d
VinIKir <vinikir@yahoo.com.br>
2024-11-06 15:55:02 -0300

    ajustes

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
    
-    //baseURL:"http://18.222.164.74/",
-    baseURL:"http://127.0.0.1:3300/",
+    baseURL:"http://18.222.164.74/",
+    //baseURL:"http://127.0.0.1:3300/",
   
     timeout: 30000,
     headers: {
